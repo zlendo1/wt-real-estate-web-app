5bce2f330e29ccb8520bf4b4b72cbaad65eb2df3
zlendo1 <zlendo1@etf.unsa.ba>
2023-11-25 18:06:04 +0100

    prep work for implementation

---
+++ scripts/SpisakNekratnina.js
@@ -0,0 +1,26 @@
+let SpisakNekretnina = function () {
+    //privatni atributi modula
+    let listaNekretnina = [];
+    let listaKorisnika = [];
+
+
+    //implementacija metoda
+    let init = function (listaNekretnina, listaKorisnika) {
+        // dodajte kod
+    }
+
+    let filtrirajNekretnine = function (kriterij) {
+        // dodajte kod
+    }
+
+    let ucitajDetaljeNekretnine = function (id) {
+        // dodajte kod
+    }
+
+
+    return {
+        init: init,
+        filtrirajNekretnine: filtrirajNekretnine,
+        ucitajDetaljeNekretnine: ucitajDetaljeNekretnine
+    }
+};
