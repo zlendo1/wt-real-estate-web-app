b8432fe0f64cfde75874d8ba6471c457583ec537
zlendo1 <zlendo1@etf.unsa.ba>
2023-11-26 22:54:16 +0100

    fixed filter issue

--- scripts/SpisakNekratnina.js
+++ scripts/SpisakNekratnina.js
@@ -8,7 +8,7 @@ let SpisakNekretnina = function () {
     }
 
     let filtrirajNekretnine = function (kriterij) {
-        let newList = listaNekretnina
+        let newList = this.listaNekretnina
 
         let filterIfCriterion = function (criterionName, criterionLambda) {
             if (kriterij[criterionName] !== undefined) {
@@ -17,10 +17,10 @@ let SpisakNekretnina = function () {
         }
 
         filterIfCriterion('tip_nekretnine', obj => obj['tip_nekretnine'] === kriterij['tip_nekretnine'])
-        filterIfCriterion('min_kvadratura', obj => obj['min_kvadratura'] >= kriterij['min_kvadratura'])
-        filterIfCriterion('max_kvadratura', obj => obj['max_kvadratura'] <= kriterij['max_kvadratura'])
-        filterIfCriterion('min_cijena', obj => obj['min_cijena'] >= kriterij['min_cijena'])
-        filterIfCriterion('max_cijena', obj => obj['max_cijena'] <= kriterij['max_cijena'])
+        filterIfCriterion('min_kvadratura', obj => obj['kvadratura'] >= kriterij['min_kvadratura'])
+        filterIfCriterion('max_kvadratura', obj => obj['kvadratura'] <= kriterij['max_kvadratura'])
+        filterIfCriterion('min_cijena', obj => obj['cijena'] >= kriterij['min_cijena'])
+        filterIfCriterion('max_cijena', obj => obj['cijena'] <= kriterij['max_cijena'])
 
         return newList
     }
