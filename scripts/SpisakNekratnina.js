e9b50ba72c24b8306bb23dc23e28056a24c04ec8
zlendo1 <zlendo1@etf.unsa.ba>
2023-11-26 19:33:11 +0100

    initial impl

--- scripts/SpisakNekratnina.js
+++ scripts/SpisakNekratnina.js
@@ -1,20 +1,34 @@
 let SpisakNekretnina = function () {
-    //privatni atributi modula
     let listaNekretnina = [];
     let listaKorisnika = [];
 
-
-    //implementacija metoda
     let init = function (listaNekretnina, listaKorisnika) {
-        // dodajte kod
+        this.listaNekretnina = listaNekretnina
+        this.listaKorisnika = listaKorisnika
     }
 
     let filtrirajNekretnine = function (kriterij) {
-        // dodajte kod
+        let newList = listaNekretnina
+
+        let filterIfCriterion = function (criterionName, criterionLambda) {
+            if (kriterij[criterionName] !== undefined) {
+                newList = newList.filter(criterionLambda)
+            }
+        }
+
+        filterIfCriterion('tip_nekretnine', obj => obj['tip_nekretnine'] === kriterij['tip_nekretnine'])
+        filterIfCriterion('min_kvadratura', obj => obj['min_kvadratura'] >= kriterij['min_kvadratura'])
+        filterIfCriterion('max_kvadratura', obj => obj['max_kvadratura'] <= kriterij['max_kvadratura'])
+        filterIfCriterion('min_cijena', obj => obj['min_cijena'] >= kriterij['min_cijena'])
+        filterIfCriterion('max_cijena', obj => obj['max_cijena'] <= kriterij['max_cijena'])
+
+        return newList
     }
 
     let ucitajDetaljeNekretnine = function (id) {
-        // dodajte kod
+        let result = listaNekretnina.find(obj => obj.id === id)
+
+        return result !== undefined ? result : null
     }
 
 
