6137aa8bc615ca15a2b8b98109a56170cb636ae5
zlendo1 <zlendo1@etf.unsa.ba>
2023-12-21 16:56:47 +0100

    implemented last two requests

--- index.js
+++ index.js
@@ -15,8 +15,24 @@ const bcrypt = require("bcrypt")
 
 const fs = require("fs-extra")
 
-let korisnici = import("./data/korisnici.json", { assert: { type: "json" } })
-let nekretnine = import("./data/nekretnine.json", { assert: { type: "json" } })
+const pathKorisnici = "./data/korisnici.json"
+const pathNekretnine = "./data/nekretnine.json"
+
+function readKorisnici() {
+    return fs.readJson(pathKorisnici)
+}
+
+function readNekretnine() {
+    return fs.readJson(pathNekretnine)
+}
+
+function writeKorisnici(korisnici) {
+    return fs.writeJson(pathKorisnici, JSON.stringify(korisnici))
+}
+
+function writeNekretnine(nekretnine) {
+    return fs.writeJson(pathNekretnine, JSON.stringify(nekretnine))
+}
 
 // Routes
 app.post('/login', (req, res) => {
@@ -34,7 +50,7 @@ app.post('/login', (req, res) => {
             return
         }
 
-        korisnici
+        readKorisnici()
             .then(korisnici => {
                 const matchingUser = korisnici.find(
                     korisnik => korisnik.username === bodyUsername && korisnik.password === hash
@@ -113,7 +129,7 @@ app.post("/upit", (req, res) => {
     const nekretnina_id = requestBody["nekretnina_id"]
     const tekstUpita = requestBody["tekst_upita"]
 
-    nekretnine
+    readNekretnine()
         .then(nekretnine => {
             let nekretnina = nekretnine.find(nekretnina => nekretnina.id === nekretnina_id)
 
@@ -130,7 +146,7 @@ app.post("/upit", (req, res) => {
                 tekst_upita: tekstUpita
             })
 
-            fs.writeJson("./data/nekretnine.json", JSON.stringify(nekretnine))
+            writeNekretnine(nekretnine)
                 .then(() => {
                     res.status(200).send(
                         {poruka: "Upit je uspješno dodan"}
@@ -141,6 +157,93 @@ app.post("/upit", (req, res) => {
                         {greska: "Greška u čitanju nekretnina"}
                     )
                 })
+        })
+        .catch(err => {
+            res.status(500).send(
+                {greska: "Greška u čitanju nekretnina"}
+            )
+        })
+})
+
+app.put("/korisnik", (req, res) => {
+    if (!req.session.user) {
+        res.status(401).send(
+            {greska: "Neautorizovan pristup"}
+        )
+
+        return
+    }
+
+    const requestBody = req.body
+
+    const bodyIme = requestBody["ime"]
+    const bodyPrezime = requestBody["prezime"]
+    const bodyUsername = requestBody["username"]
+    const bodyPassword = requestBody["password"]
+
+    readKorisnici()
+        .then(korisnici => {
+            let matchingUser = korisnici.find(korisnik => korisnik.id === req.session.user.id)
+
+            if (!matchingUser) {
+                res.status(500).send(
+                    {greska: `Session user ne postoji u bazi`}
+                )
+
+                return
+            }
+
+            if (bodyIme) {
+                matchingUser.ime = bodyIme
+            }
+
+            if (bodyPrezime) {
+                matchingUser.prezime = bodyPrezime
+            }
+
+            if (bodyUsername) {
+                matchingUser.username = bodyUsername
+            }
+
+            if (bodyPassword) {
+                bcrypt.hash(bodyPassword, 10, (err, hash) => {
+                    if (err) {
+                        res.status(500).send(
+                            {greska: "Greška u enkripciji lozinke"}
+                        )
+
+                        return
+                    }
+
+                    matchingUser.password = hash
+                })
+            }
+
+            writeKorisnici(korisnici)
+                .then(() => {
+                    res.status(200).send(
+                        {poruka: "Podaci su uspješno ažurirani"}
+                    )
+                })
+                .catch(err => {
+                    res.status(500).send(
+                        {greska: "Greška u čitanju korisnika"}
+                    )
+                })
+        })
+        .catch(err => {
+            res.status(500).send(
+                {greska: "Greška u čitanju korisnika"}
+            )
+        })
+})
+
+app.get("/nekretnine", (req, res) => {
+    readNekretnine()
+        .then(nekretnine => {
+            res.status(200).send(
+                nekretnine
+            )
         })
         .catch(err => {
             res.status(500).send(
