56067a66926611f7c8f24c8d6c0df9c872bb5540
zlendo1 <zlendo1@etf.unsa.ba>
2023-12-23 17:36:40 +0100

    implemented PozoviAjax methods

--- ajax.js
+++ ajax.js
@@ -7,31 +7,66 @@ const PoziviAjax = (() => {
     // ako postoji greška, poruka se prosljeđuje u error parametru
     // callback-a, a data je tada null
 
+    function callAjax(method, url, fnCallback, data = null) {
+        const ROOT = "http://localhost:3000"
+
+        let ajax = new XMLHttpRequest()
+
+        ajax.onreadystatechange = () => {
+            if (ajax.readyState === 4) {
+                const jsonRes = JSON.parse(ajax.responseText)
+
+                if (ajax.status === 200) {
+                    fnCallback(null, jsonRes)
+                } else if (ajax.status === 500) {
+                    console.log(`Greška na serveru: ${jsonRes.greska}`)
+                } else {
+                    fnCallback(jsonRes, null)
+                }
+            }
+        }
+
+        ajax.open(method, `${ROOT}${url}`)
+        ajax.setRequestHeader("Content-Type", "application/json")
+
+        if (data) {
+            ajax.send(JSON.stringify(data))
+        } else {
+            ajax.send()
+        }
+    }
+
     // vraća korisnika koji je trenutno prijavljen na sistem
     function impl_getKorisnik(fnCallback) {
-
+        callAjax("GET", "/korisnik", fnCallback)
     }
 
     // ažurira podatke loginovanog korisnika
     function impl_putKorisnik(noviPodaci, fnCallback) {
-
+        callAjax("PUT", "/korisnik", fnCallback, noviPodaci)
     }
 
     // dodaje novi upit za trenutno loginovanog korisnika
     function impl_postUpit(nekretnina_id, tekst_upita, fnCallback) {
-
+        callAjax("POST", "/upit", fnCallback, {
+            nekretnina_id: nekretnina_id,
+            tekst_upita: tekst_upita
+        })
     }
 
     function impl_getNekretnine(fnCallback) {
-
+        callAjax("GET", "/nekretnine", fnCallback)
     }
 
     function impl_postLogin(username, password, fnCallback) {
-
+        callAjax("POST", "/login", fnCallback, {
+            username: username,
+            password: password
+        })
     }
 
     function impl_postLogout(fnCallback) {
-
+        callAjax("POST", "/logout", fnCallback)
     }
 
     return {
@@ -41,5 +76,5 @@ const PoziviAjax = (() => {
         putKorisnik: impl_putKorisnik,
         postUpit: impl_postUpit,
         getNekretnine: impl_getNekretnine
-    };
-})();
+    }
+})()
