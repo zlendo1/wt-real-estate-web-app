c5c8bd3ef92901669c4e20315d3e61ae9ab2f145
zlendo1 <zlendo1@etf.unsa.ba>
2023-12-27 22:22:18 +0100

    added MarketingAjax.js

---
+++ public/scripts/MarketingAjax.js
@@ -0,0 +1,16 @@
+const MarketingAjax = (() => {
+    function impl_novoFiltriranje() {}
+
+    function impl_klikNekretnina() {}
+
+    function impl_osvjeziPretrage() {}
+
+    function impl_osvjeziKlikove() {}
+
+    return {
+        novoFiltriranje: impl_novoFiltriranje,
+        klikNekretnina: impl_klikNekretnina,
+        osvjeziPretrage: impl_osvjeziPretrage,
+        osvjeziKlikove: impl_osvjeziKlikove
+    }
+})()
